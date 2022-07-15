f0e7c6cd21e35e5bbccfcd8da3e8d67a85153597
chungs10 <chungs10@rpi.edu>
2022-07-14 21:12:51 -0400

    Moving into things

--- SJF.py
+++ SJF.py
@@ -18,6 +18,55 @@ F
 """
 from GenProcesses import *
 
+def allIndexOfTargerNum(targetNum, list):
+    allIndex = []
+    for i in range(len(list)):
+        if (targetNum == list[i]):
+            allIndex.append(i)
+            
+    return allIndex
+
+def SJF(Processes, contextSwitchTime):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    # tell whenever the algo is done so the timer stop
+    allProcessTerminate = False
+    
+    # In charge of how many processes
+    numProcess = Processes.self.num_process_ 
+
+    # a LIST that keep track of the time that the next process arrive. Pop arrival_Time[0] out once timer = arrival_Time[0]
+    arrivalTimer = Processes.self.arrival_Time
+    
+    # a dict of all of the info of all processes
+    processesInfo = Processes.self.reorganizedData
+    
+
+    # list of processes in the ready queue
+    ready_queue = []
+    
+    # list of processes in the IO Burst State
+    IOqueue = []
+    
+    # list of processes in the CPU Burst State
+    CPUqueue = []
+    
+    # list of processes finish both IO and CPU Burst
+    completed = []
+    
+    print("time 0ms: Simulator started for SJF [Q: empty]")
+    timer  = 0
+    while(not allProcessTerminate):
+        # if there are still Processes left to arrive, print that out
+        processesNameList = allIndexOfTargerNum(timer, arrivalTimer)
+        if(numProcess > 0 and len(processesNameList) > 0):
+            # add all of the processes to the queue
+            for i in range(len(processesNameList)):
+                ready_queue.append()
+        
+        timer += 1
+    
+    
+
 
 if __name__ == "__main__":
     print("yeeet")
@@ -26,3 +75,4 @@ if __name__ == "__main__":
     test_Process.reorganizeData()
     test_Process.printReorganizedData()
     #test_Process.print()
+

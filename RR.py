4a3879b43970c1ff92245ac13c2acf2c6183f83f
QuackGameDev <107073102+QuackGameDev@users.noreply.github.com>
2022-07-15 22:45:03 -0400

    Stuff

--- RR.py
+++ RR.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Jul 10 21:59:20 2022
 
-@author: Quang Nguyen
+@author: Quang Nguyen, Roger Luo
 """
 # parameter: pass in a Processes class
 def RR(processes, contextSwitchTime, timeSlice):
@@ -10,5 +10,10 @@ def RR(processes, contextSwitchTime, timeSlice):
     processing = []
     completed = []
     allCompleted = False # keep track of when all of the processes is completed or not
+    arrTime = Processes.arrival_Time
+    cpuBursts = copy.deepcopy(Processes.CPU_Burst)
+    ioBursts = copy.deepcopy(Processes.IO_Burst)
+    numBursts = copy.deepcopy(Processes.num_Burst)
+    cSwitches = 0 #The number of times that context switching occurs
     print("time 0ms: Simulator started for RR with time slice {ts}ms [Q: empty]".format(ts = timeSlice))
     timer = 0
--- SRT.py
+++ SRT.py
@@ -7,8 +7,7 @@ Created on Sun Jul 10 21:59:20 2022
 from GenProcesses import *
 import copy
 
-def add2Q(Q, Process):
-    Q.append(Process)
+
     
 def SRT(Processes, contextSwitch, alpha):
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -18,12 +17,31 @@ def SRT(Processes, contextSwitch, alpha):
     preemptions = 0
     procLeft = Processes.num_process_
     arrTime = Processes.arrival_Time
-    procCopy = copy.deepcopy(Processes.reorganizedData)
+    cpuBursts = copy.deepcopy(Processes.CPU_Burst)
+    ioBursts = copy.deepcopy(Processes.IO_Burst)
+    numBursts = copy.deepcopy(Processes.num_Burst)
+    taus = [Processes.tau for x in range(procLeft)]
     AllDone = False
+    Curr = "" #The current process the CPU is working on
     completed = [] #Which processes are done
     IO = [] #What is in the IO
     cSwitches = 0 #The number of times that context switching occurs
-
+    buffer = contextSwitch
 
     print("time 0ms: Simulator started for SRT ", end = "")
     printQueue(Q)
+    while(procLeft > 0):
+        for x in arrTime:
+            if(x == time):
+                Q.append(alphabet[arrTime.index(x)])
+                print("time ", time, "ms: Process ", alphabet[arrTime.index(x)], " (tau ", taus[arrTime.index(x)], "ms) arrived; added to ready queue ", end = "", sep = "")
+                printQueue(Q)
+        if(buffer == 0):
+            buffer = contextSwitch
+            Working = True
+
+        if(Working == False):
+            buffer -= 1
+            continue
+    
+        time += 1

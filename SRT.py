81222b7cd35d5307f2ed112e31c233c3e581d669
QuackGameDev <107073102+QuackGameDev@users.noreply.github.com>
2022-07-15 20:07:02 -0400

    l

--- SRT.py
+++ SRT.py
@@ -7,17 +7,23 @@ Created on Sun Jul 10 21:59:20 2022
 from GenProcesses import *
 import copy
 
+def add2Q(Q, Process):
+    Q.append(Process)
+    
 def SRT(Processes, contextSwitch, alpha):
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     
     Q = [] #The Queue
     time = 0 #The number of milliseconds that has passed
     preemptions = 0
-    Working = False
     procLeft = Processes.num_process_
     arrTime = Processes.arrival_Time
     procCopy = copy.deepcopy(Processes.reorganizedData)
+    AllDone = False
+    completed = [] #Which processes are done
+    IO = [] #What is in the IO
+    cSwitches = 0 #The number of times that context switching occurs
+
 
     print("time 0ms: Simulator started for SRT ", end = "")
     printQueue(Q)
-    
